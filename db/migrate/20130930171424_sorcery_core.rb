5307e13cee8ff050a92361713679a0ddfd14764e
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-09-30 14:05:40 -0400

    Generating Project model with validations

--- db/migrate/20130930171424_sorcery_core.rb
+++ db/migrate/20130930171424_sorcery_core.rb
@@ -1,8 +1,8 @@
 class SorceryCore < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :first_name,       :default => nil
-      t.string :last_name,        :default => nil
+      t.string :first_name,       :null => false  # if you use another field as a username, for example email, you can safely remove this field.
+      t.string :last_name,       :null => false 
       t.string :email,            :default => nil # if you use this field as a username, you might want to make it :null => false.
       t.string :crypted_password, :default => nil
       t.string :salt,             :default => nil
