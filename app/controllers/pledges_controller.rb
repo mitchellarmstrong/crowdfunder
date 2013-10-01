b53edd1031e186d16fa213630161c9643b01d775
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-10-01 14:10:53 -0400

    Making tests for pledging pass

---
+++ app/controllers/pledges_controller.rb
@@ -0,0 +1,25 @@
+class PledgesController < ApplicationController
+	before filter :require_login
+	before filter :require_project
+
+	def new
+		@pledge = @project.pledges.build
+	end
+
+	def create
+		@pledge = @project.pledges.build params[:pledge]
+		@pledge.user = current_user
+
+		if @pledge.save
+			redirect_to @project, notice: "Nice! Thanks for pledging $#{@pledge.amount} for this project."
+		else
+			render :new
+		end
+	end
+
+	protected
+
+	def require_project
+		@project = Project.find params[:project_id]
+	end
+end
